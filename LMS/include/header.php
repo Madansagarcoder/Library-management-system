508386ee393f375668d2a2942c06122fd5b7f790
Madan Sagar <144838155+Madansagarcoder@users.noreply.github.com>
2025-04-07 21:26:27 +0530

    Add files via upload

---
+++ LMS/include/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="<?php echo BASE_URL ?>assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="<?php echo BASE_URL ?>assets/css/style.css">
+    <script src="<?php echo BASE_URL ?>assets/js/7d9b19adfc.js" crossorigin="anonymous"></script>
+    <title>Dashboard | Library</title>
+</head>
+
+<body>
